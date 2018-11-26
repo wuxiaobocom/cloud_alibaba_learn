eb4461e606c0dc296930bc471f53f4052f26120c
wuxiaobo <wuxiaobo@didachuxing.com>
2018-11-26 22:09:50 +0800

    初始化项目

---
+++ sentinel_demo/src/main/java/com/bobo/cloudalibaba/SentinelDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bobo.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelDemoApplication.class, args);
+	}
+}
