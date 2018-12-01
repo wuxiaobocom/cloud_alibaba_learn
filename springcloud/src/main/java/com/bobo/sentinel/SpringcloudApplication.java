07aefe11da78e7be8074a7817f8e3b7c9dc2e5ae
wuxiaobo <wuxiaobo@didachuxing.com>
2018-12-01 10:30:45 +0800

    添加新文件

---
+++ springcloud/src/main/java/com/bobo/sentinel/SpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.bobo.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+}
